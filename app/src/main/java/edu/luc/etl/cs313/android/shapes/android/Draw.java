aaf982f067d9d5af6a83245870b5d2d5eebb1f39
paytongithub <trp31604@gmail.com>
2025-03-12 15:43:26 -0500

    added onLocation, onOutline, onFill, and onStrokeColor to code - Layla

--- app/src/main/java/edu/luc/etl/cs313/android/shapes/android/Draw.java
+++ app/src/main/java/edu/luc/etl/cs313/android/shapes/android/Draw.java
@@ -33,13 +33,21 @@ public class Draw implements Visitor<Void> {
 
     @Override
     public Void onStrokeColor(final StrokeColor c) {
+        int originalColor = paint.getColor();
+        paint.setColor(c.getColor());
 
+        c.getShape().accept(this);
+
+        paint.setColor(originalColor);
         return null;
     }
 
     @Override
     public Void onFill(final Fill f) {
-
+        Paint.Style originalStyle = paint.getStyle();
+        paint.setStyle(Style.FILL_AND_STROKE);
+        f.getShape().accept(this);
+        paint.setStyle(originalStyle);
         return null;
     }
 
@@ -53,7 +61,10 @@ public class Draw implements Visitor<Void> {
 
     @Override
     public Void onLocation(final Location l) {
-
+        canvas.save();
+        canvas.translate(l.getX(), l.getY());
+        l.getShape().accept(this);
+        canvas.restore();
         return null;
     }
 
@@ -65,7 +76,10 @@ public class Draw implements Visitor<Void> {
 
     @Override
     public Void onOutline(Outline o) {
-
+        Paint.Style originalStyle = paint.getStyle();
+        paint.setStyle(Style.STROKE);
+        o.getShape().accept(this);
+        paint.setStyle(originalStyle);
         return null;
     }
 
